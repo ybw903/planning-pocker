012778659c90dcb08a6c56478feb44412ba46bd5
유병우 <conner903@naver.com>
2023-01-05 22:32:55 +0900

    style: 로그인 화면 퍼블리싱

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,18 +1,60 @@
 import {useState} from 'react';
-import {Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {Button, TextField} from '../../components';
+import common from '../../styles/common';
 
 const LoginScreen: React.FC = () => {
   const [account, setAccount] = useState('');
   const [password, setPassword] = useState('');
+
   return (
-    <View>
-      <TextInput value={account} onChangeText={setAccount} />
-      <TextInput value={password} onChangeText={setPassword} />
-      <TouchableOpacity>
-        <Text>로그인</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.formContainer}>
+        <Text style={styles.label}>로그인</Text>
+        <TextField
+          value={account}
+          onChangeText={setAccount}
+          style={styles.mgBt}
+        />
+        <Text style={styles.label}>비밀번호</Text>
+        <TextField
+          value={password}
+          onChangeText={setPassword}
+          style={styles.mgBt}
+        />
+        <Button style={styles.mgT}>
+          <Text style={{color: '#fff'}}>로그인</Text>
+        </Button>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  formContainer: {
+    backgroundColor: '#eee',
+    width: 270,
+    height: 300,
+    borderRadius: 10,
+    padding: 40,
+    ...common.shadow,
+  },
+  label: {
+    marginBottom: 5,
+    fontWeight: '600',
+  },
+  mgBt: {
+    marginBottom: 10,
+  },
+  mgT: {
+    marginTop: 40,
+  },
+});
 
 export default LoginScreen;
